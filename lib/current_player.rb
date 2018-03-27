443bbb2614297f3f56deba4d9d03c7af51f50ecd
Alejandro Sabogal <asabogal.git@gmail.com>
2018-03-27 00:07:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
-
-def turn_count(board)
-board.each do |index|
-  turn_number = 0
-  if index != " "
-    turn_number += 1
-  end
-end
-    
+#
+# def turn_count(board)
+# board.each do |index|
+#   turn_number = 0
+#   if index != " "
+#     turn_number += 1
+#   end
+# end
+#
