301a7a73883e122c505bdb21e5f866161f9cc4f1
Alejandro Sabogal <asabogal.git@gmail.com>
2018-03-27 00:32:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,7 +10,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 = 0
+  player = turn_count(board).to_i
+  if player % 2 = 0
     return "X"
     else
     return "O"
